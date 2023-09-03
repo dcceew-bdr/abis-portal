4fdc2a5769e1e19e4d4da0bb90be814f59838c88
Edmond Chuc <edmond@kurrawong.ai>
2023-09-03 15:15:33 +1000

    Update About page content

--- geochem-portal-ui/src/views/AboutView.vue
+++ geochem-portal-ui/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
-    <h1 class="text-2xl font-bold">This is an about page</h1>
+    <h1 class="text-2xl font-bold">About</h1>
+    <p>This is a validation system for GSQ geochemistry data.</p>
   </main>
 </template>
