5f77bedda60dccdf92d672f1a6f47c7ccbcf781b
Edmond Chuc <edmond@kurrawong.ai>
2023-09-07 14:34:27 +1000

    Update about page h1 style

--- geochem-portal-ui/src/views/AboutView.vue
+++ geochem-portal-ui/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <h1 class="text-2xl font-bold">About</h1>
+    <h1 class="text-4xl pb-4">About</h1>
     <p>This is a validation system for GSQ geochemistry data.</p>
   </main>
 </template>
