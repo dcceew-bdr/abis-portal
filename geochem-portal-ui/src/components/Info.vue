b858553050d7116850e7ff33e8b376992466e7a2
Edmond Chuc <edmond@kurrawong.ai>
2023-09-28 12:34:10 +1000

    Update info panel with link to GSQ Geochem spec

--- geochem-portal-ui/src/components/Info.vue
+++ geochem-portal-ui/src/components/Info.vue
@@ -10,30 +10,30 @@ const props = defineProps<{ large: boolean }>()
       <article>
         <h3 class="text-base">Data Submission Formats</h3>
         <ul class="list-disc pl-[2rem]">
-          <li><a href="#">GSQ Excel template</a></li>
-          <li><a href="#">JSON</a></li>
-          <li><a href="#">RDF</a></li>
+          <li><a href="#">GSQ Excel template (placeholder)</a></li>
+          <li><a href="#">JSON (placeholder)</a></li>
+          <li><a href="#">RDF (placeholder)</a></li>
         </ul>
       </article>
 
       <article>
         <h3 class="text-base">Geochemistry Standards</h3>
         <ul class="list-disc pl-[2rem]">
-          <li><a href="#">Conceptual Model</a></li>
+          <li><a href="http://linked.data.gov.au/def/geochem" target="_blank">GSQ Geochemistry Data Exchange Model - Specification <i class="pi pi-external-link"></i></a></li>
         </ul>
       </article>
 
       <article>
         <h3 class="text-base">OpenAPI Specification</h3>
         <ul class="list-disc pl-[2rem]">
-          <li><a href="#">Geochemistry Validation API</a></li>
+          <li><a href="#">Geochemistry Validation API (placeholder)</a></li>
         </ul>
       </article>
 
       <article>
         <h3 class="text-base">System Information</h3>
         <ul class="list-disc pl-[2rem]">
-          <li><a href="#">Version 0.1.0-dev.0</a></li>
+          <li><a href="#">Version 0.1.0-dev.0 (placeholder)</a></li>
         </ul>
       </article>
     </article>
