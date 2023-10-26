21067e65003fa3fef26b26167d4641cd1a5a08ed
Edmond Chuc <edmond@kurrawong.ai>
2023-10-26 19:02:43 +1000

    Remove dcceew header overlap

--- geochem-portal-ui/src/components/Header.vue
+++ geochem-portal-ui/src/components/Header.vue
@@ -1,22 +1,17 @@
 <template>
   <header id="header" role="banner">
     <div class="wrapper">
-      <div class="region region-header">
+      <!-- <div class="region region-header">
         <div class="region-header-inner-wrapper">
           <div
             id="block-system-branding-block"
             class="block block-system block-system-branding-block"
           >
-            <div>
-              <a href="/" title="Home" rel="home">
-
-              </a>
-            </div>
           </div>
           
           
         </div>
-      </div>
+      </div> -->
     </div>
     <!-- .wrapper -->
   </header>
