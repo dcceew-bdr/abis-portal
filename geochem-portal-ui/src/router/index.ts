7931dc96644df8236f9bfc44b9a5de00c82b394d
Edmond Chuc <edmond@kurrawong.ai>
2023-10-26 19:07:01 +1000

    Fix router component import style breaking in prod

--- geochem-portal-ui/src/router/index.ts
+++ geochem-portal-ui/src/router/index.ts
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ProfilesView from '../views/ProfilesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +6,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/profiles',
       name: 'profiles',
-      component: () => ProfilesView
+      component: () => import('../views/ProfilesView.vue')
     }
   ]
 })
